f2dbd6f7661f2db8a42d4103b33d709be0bca589
porto <porto@gmail.com>
2011-10-07 11:54:21 +0200

    Added support to List<> conversion with Gson

--- src/com/coctelmental/android/project1886/common/util/JsonHandler.java
+++ src/com/coctelmental/android/project1886/common/util/JsonHandler.java
@@ -1,5 +1,7 @@
 package com.coctelmental.android.project1886.common.util;
 
+import java.lang.reflect.Type;
+
 import com.google.gson.Gson;
 
 public class JsonHandler {
@@ -9,11 +11,20 @@ public class JsonHandler {
 		Gson gson = new Gson();
 		return gson.toJson(object);
 	}
-
+	
+	public static String toJson(Object object, Type type) {
+		Gson gson = new Gson();
+		return gson.toJson(object, type);
+	}
 
 	public static <T> T fromJson(String jsonObject, Class<T> classType) {
 		Gson gson = new Gson();
 		return gson.fromJson(jsonObject, classType);
+	}
+	
+	public static <T> T fromJson(String jsonObject, Type type) {
+		Gson gson = new Gson();
+		return gson.fromJson(jsonObject, type);
 	}
 
 }
