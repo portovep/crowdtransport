6bdbc0a3ce5b77c7cf91861e0ff7fa6950fc8877
porto <porto.vga@gmail.com>
2011-12-12 17:43:10 +0100

    Added timeout to GET operations.

--- src/com/coctelmental/android/project1886/util/ConnectionsHandler.java
+++ src/com/coctelmental/android/project1886/util/ConnectionsHandler.java
@@ -1,7 +1,9 @@
 package com.coctelmental.android.project1886.util;
 
 
+import org.restlet.Client;
 import org.restlet.data.MediaType;
+import org.restlet.data.Protocol;
 import org.restlet.data.Status;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
@@ -13,16 +15,24 @@ import com.coctelmental.android.project1886.model.ResultBundle;
 public class ConnectionsHandler {
 	
 	private static final String SERVER_ADDRESS = "http://project1886.servehttp.com:8085/webservice";
-	//private static final String SERVER_ADDRESS = "http://192.168.43.253:8085/webservice";
+	//private static final String SERVER_ADDRESS = "http://192.168.1.140:8085/webservice";
 	
 	public static ResultBundle get(String targetURL) {
-		// create new result bundle and add default response code as 404
+		// create new result bundle
 		ResultBundle result = new ResultBundle();
+		// setup connector
+		Client client = new Client(Protocol.HTTP);
+		client.setConnectTimeout(5000); // 5s
 		ClientResource cr = new ClientResource(SERVER_ADDRESS+targetURL);
+		// attach client connector
+		cr.setNext(client);
 		try {
 			Representation r = cr.get(MediaType.APPLICATION_JSON);
 			// add response content to result bundle
 			result.setContent(r.getText());
+			
+			client.stop();
+			
 		}catch(Exception e){
 			e.printStackTrace();
 			result.setContent("");
