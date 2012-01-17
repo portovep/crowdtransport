3d351d358d69fb849fe505f31a0592a6575d1e80
porto <porto.vga@gmail.com>
2012-01-17 21:08:25 +0100

    Added route info label on TaxiDriverRouteView

--- src/com/coctelmental/android/project1886/taxis/TaxiDriverRouteView.java
+++ src/com/coctelmental/android/project1886/taxis/TaxiDriverRouteView.java
@@ -3,10 +3,16 @@ package com.coctelmental.android.project1886.taxis;
 import java.net.HttpURLConnection;
 import java.text.DecimalFormat;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -18,6 +24,8 @@ import com.coctelmental.android.project1886.R;
 import com.coctelmental.android.project1886.common.GeoPointInfo;
 import com.coctelmental.android.project1886.common.ServiceRequestInfo;
 import com.coctelmental.android.project1886.helpers.ServiceRequestsHelper;
+import com.coctelmental.android.project1886.main.Preferences;
+import com.coctelmental.android.project1886.model.ResultBundle;
 import com.coctelmental.android.project1886.util.JsonHandler;
 import com.coctelmental.android.project1886.util.Tools;
 import com.google.android.maps.GeoPoint;
@@ -33,6 +41,9 @@ public class TaxiDriverRouteView extends MapActivity {
 	private static final int INIT_ZOOM_LEVEL = 17;
 
 	private MyLocationOverlay myLocationOverlay;
+	
+	private GeoPoint gpOrigin = null;
+	private GeoPoint gpDestination = null;
 	
 	private ServiceRequestInfo serviceRequest;
 	private String jsonServiceRequest = null;
@@ -72,18 +83,15 @@ public class TaxiDriverRouteView extends MapActivity {
 	        GeoPointInfo gpOri = serviceRequest.getGpFrom();
 	        GeoPointInfo gpDest = serviceRequest.getGpTo();
 	        
+	        gpOrigin = new GeoPoint(gpOri.getLatitudeE6(), gpOri.getLongitudeE6());
+	        gpDestination = new GeoPoint(gpDest.getLatitudeE6(), gpDest.getLongitudeE6());
+	       
+			// update route info labels
+			new GetRouteInfoAsyncTask().execute();
+	        
 	        // setup overlays
-	        OverlayItem overlayOri = new OverlayItem(new GeoPoint(gpOri.getLatitudeE6(), gpOri.getLongitudeE6()),
-	        			"", "");       
-	        OverlayItem overlayDest = new OverlayItem(new GeoPoint(gpDest.getLatitudeE6(), gpDest.getLongitudeE6()),
-	    				"", "");
-        	
-			// calculating distance
-			Double distance = Tools.calculateDistanceInMeters(overlayOri.getPoint(), overlayDest.getPoint());
-			// update distance label
-			TextView tvDistance = (TextView) findViewById(R.id.distance);
-			DecimalFormat df = new DecimalFormat("#######0.0#");
-			tvDistance.setText(" "+df.format(distance)+"m");
+	        OverlayItem overlayOri = new OverlayItem(gpOrigin, "", "");       
+	        OverlayItem overlayDest = new OverlayItem(gpDestination, "", "");
 	        
 	        // add overlays
 	        RouteItemizedOverlay itemizedOverlay = new RouteItemizedOverlay(getResources().getDrawable(R.drawable.marker_ori));
@@ -172,6 +180,76 @@ public class TaxiDriverRouteView extends MapActivity {
 				toast.show();	
 	        	Log.d("ServiceRequest", "Error trying accept ServiceRequest in server" +
 	        			"Error code -> (" + result + ")");
+	        }
+	    }
+	}
+	
+	private void updateDistanceLabel(double distanceInMeters) {		
+		// check user preferences
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		String units = sp.getString(Preferences.PREF_DISTANCE_UNITS, Preferences.DEFAULT_DISTANCE_UNITS);
+		
+		if (units.equals("km"))
+			// parse to kilometers
+			distanceInMeters = distanceInMeters / 1000;
+		// update label
+		DecimalFormat df = new DecimalFormat("#######0.0#");
+		TextView tvDistance = (TextView) findViewById(R.id.distance);
+		tvDistance.setText(" " + df.format(distanceInMeters) + units);
+	}
+	
+	private void updateRouteTimeLabel(String timeText) {
+		TextView tvTime = (TextView) findViewById(R.id.time);
+		tvTime.setText("   " + timeText);
+	}
+	
+	private class GetRouteInfoAsyncTask extends AsyncTask<Void, Void, ResultBundle> {
+		
+	    protected ResultBundle doInBackground(Void... params) {
+	        return ServiceRequestsHelper.obtainRouteInfo(gpOrigin, gpDestination);
+	    }
+
+	    protected void onPostExecute(ResultBundle rb) {
+	    	boolean error = true;
+	        // check response code
+	        if(rb.getResultCode() == HttpURLConnection.HTTP_OK) {
+	        	String jsonRouteInfo = rb.getContent();
+	        	
+	        	try{
+	        		// parse JSON route info data
+		        	JSONObject joRouteInfo = new JSONObject(jsonRouteInfo);
+		        	
+		        	// check response code
+		        	String status = joRouteInfo.getString("status");
+		        	if (status.equals("OK")) {
+		        		// disable error flag
+			        	error = false;
+			        	JSONArray rows = joRouteInfo.getJSONArray("rows");
+			        	// get distance in meters
+			        	double distance = (double) rows.getJSONObject(0).getJSONArray("elements")
+			        			.getJSONObject(0).getJSONObject("distance").getDouble("value");
+			        	// get time text
+			        	String timeText = (String) rows.getJSONObject(0).getJSONArray("elements")
+			        			.getJSONObject(0).getJSONObject("duration").getString("text");		
+			        	
+			        	Log.d("ROUTE_INFO", "Route distance: " + distance);
+			        	Log.d("ROUTE_INFO", "Route time: " + timeText);
+			        	
+			        	// update labels
+			        	updateDistanceLabel(distance);
+			        	updateRouteTimeLabel(timeText);
+		        	}
+	        	}catch (JSONException je) {
+					Log.w("JSON error", je.getMessage());
+				}	        	
+	        }				
+	        
+	        if (error) {
+	    		// calculate unaccurate distance
+	    		Double distanceInMeters = Tools.calculateDistanceInMeters(gpOrigin, gpDestination);
+	    		// show unaccurate distance
+	    		updateDistanceLabel(distanceInMeters);
+	    		updateRouteTimeLabel(getString(R.string.routeTimeLabelDefault));
 	        }
 	    }
 	}
